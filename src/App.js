161168618d53b0f96e27540ad46ed4e935c33166
StefCS <stefanocartacs@gmail.com>
2025-02-20 19:52:34 -0500

    Attempt 1 to deploy React App to GitHub pages.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This page is coming soon! This is a React Test for GitHub Pages by StefCS.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.github.com/StefCS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit StefCS's GitHub to see more repositories.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
